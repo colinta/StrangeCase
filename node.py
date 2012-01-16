5e5b328983185039f73733171d459b36f06737f5
Colin Thomas-Arnold <colinta@mac.com>
2012-01-15 17:27:07 -0700

    getting awesome...!

--- node.py
+++ node.py
@@ -1,4 +1,10 @@
+from jinja2 import Environment
+from strange_case_jinja import YamlFrontMatterLoader, YamlFrontMatterTemplate
 import os
+
+
+env = Environment(loader=YamlFrontMatterLoader(os.getcwd()))
+env.template_class = YamlFrontMatterTemplate
 
 
 class Node(object):
@@ -26,14 +32,14 @@ class Node(object):
             self.children[child.name] = child
             self.children_list.append(child)
 
-    def __iter__(self):
-        return iter(self.children)
-
     def __nonzero__(self):
         return True
 
     def __len__(self):
-        return len(self.children)
+        return len(self.children_list)
+
+    def __iter__(self):
+        return iter(self.children_list)
 
     def __getattr__(self, key):
         conf = self.config.get(key)
@@ -43,6 +49,9 @@ class Node(object):
         if page:
             return page
         return None
+
+    def __repr__(self, indent=''):
+        return "%s (%s)" % (self.name, str(type(self)))
 
 
 class FolderNode(Node):
@@ -68,28 +77,29 @@ class PageNode(Node):
         self.target = target
 
 
-class AssetPageNode(Node):
+class AssetPageNode(PageNode):
     """
     Copies a file to a destination
     """
-    def __init__(self, parent, name, config, target, source):
+    def __init__(self, parent, name, config, target, path):
         super(AssetPageNode, self).__init__(parent, name, config, target)
-        self.source = source
+        self.path = path
 
     def build(self, **context):
         super(FolderNode, self).build(**context)
 
 
-class TemplatePageNode(PageNode):
+class TemplatePageNode(AssetPageNode):
     """
     A TemplatePageNode object is rendered before copied to its destination
     """
-    def __init__(self, parent, name, config, target, template):
-        super(TemplatePageNode, self).__init__(parent, name, config, target)
-        self.template = template
+    def __init__(self, parent, name, config, target, path):
+        super(TemplatePageNode, self).__init__(parent, name, config, target, path)
+        self.template = env.get_template(path)
+
+        self.config.update(self.template.context)
 
     def build(self, **context):
-        self.config.update(self.template.context)
-        content = self.template.render(context)
+        content = self.template.render(self.config, **context)
         with open(self.target, 'w') as dest:
             dest.write(content)
--- strange_case.py
+++ strange_case.py
@@ -2,40 +2,34 @@ import yaml
 import os
 from fnmatch import fnmatch
 from copy import deepcopy
-from jinja2 import Environment
-from strange_case_jinja import YamlFrontMatterLoader, YamlFrontMatterTemplate
 
 from node import *  # imports Node, Page, TemplatePage, and Folder
 
-
-DEFAULT_CONFIG = {
-    'rename_extensions': {
-        '.j2': '.html'
-    },
-    'ignore': [],  # CONFIG_FILE gets added to this
-    # 'dont_process': [
-    #     '*.js', '*.png', '*.jpg', '*.jpeg',
-    #     '*.css'
-    # ]
-}
-
-
-# these will eventually be command-line configurable
+# these will eventually be command-line configurable:
 CONFIG_FILE = u'config.yaml'
 CONFIG_PATH = u'./' + CONFIG_FILE
 CONTENT_PATH = u'content/'
 DEPLOY_PATH = u'public/'
+PORT = 8000
 
-DEFAULT_CONFIG['ignore'] = [CONFIG_FILE]
+DEFAULT_CONFIG = {
+    'host': 'http://localhost:' + str(PORT),
+    'rename_extensions': {
+        '.j2': '.html',
+        '.jinja2': '.html',
+    },
+    'ignore': [
+        CONFIG_FILE
+    ],
+    'dont_process': [
+        '*.js', '*.css',
+    ]
+}
 
-loader = YamlFrontMatterLoader(os.getcwd())
-env = Environment(loader=loader)
-env.template_class = YamlFrontMatterTemplate
 
-
-def build_templates_in(content_path, target_path, config, parent_node):
-    # don't modify folder_config
-    folder_config = deepcopy(config)
+def build_node_tree(content_path, target_path, config, parent_node):
+    # don't modify node_config
+    node_config = deepcopy(config)
 
     # merge folder config
     config_path = os.path.join(content_path, CONFIG_FILE)
@@ -43,40 +37,50 @@ def build_templates_in(content_path, target_path, config, parent_node):
         with open(config_path, 'r') as config_file:
             yaml_config = yaml.load(config_file)
             if yaml_config:
-                folder_config.update(yaml_config)
+                node_config.update(yaml_config)
 
-    if isinstance(folder_config['ignore'], bool) and folder_config['ignore']:
+    # if { ignore: true }, the entire directory is ignored
+    if isinstance(node_config['ignore'], bool) and node_config['ignore']:
         return
 
     for name in os.listdir(content_path):
-        if any(pattern for pattern in folder_config['ignore'] if fnmatch(name, pattern)):
+        if any(pattern for pattern in node_config['ignore'] if fnmatch(name, pattern)):
             continue
 
-        path = os.path.join(content_path, name)
-        target = os.path.join(target_path, name)
+        leaf_config = deepcopy(node_config)
 
-        local_config = deepcopy(folder_config)
+        path = os.path.join(content_path, name)
+        target_name, ext = os.path.splitext(name)
+        if leaf_config.get('name'):
+            target_name = leaf_config.get('name')
+        # .jinja2 should be served as .html
+        if leaf_config.get('rename_extensions', {}).get(ext):
+            ext = leaf_config['rename_extensions'].get(ext)
+        target = os.path.join(target_path, target_name + ext)
 
         if os.path.isdir(path):
-            folder_node = FolderNode(parent_node, name, local_config, target)
+            folder_node = FolderNode(parent_node, name, leaf_config, target)
 
-            build_templates_in(path, target, folder_config, folder_node)
+            build_node_tree(path, target, node_config, folder_node)
         else:
-            name, ext = os.path.splitext(name)
-            target_name, ext = os.path.splitext(target)
-            if ext in local_config['rename_extensions'].keys():
-                target = target_name + unicode(local_config['rename_extensions'][ext])
+            print target
 
-            if True:  # if 'should_process'
-                template = env.get_template(path, globals=local_config)
+            # an entire folder can be marked 'dont_process' using 'dont_process': true
+            # or it can contain a list of glob patterns
+            should_process = True
+            if isinstance(node_config['dont_process'], bool) and not node_config['dont_process']:
+                should_process = False
+            elif any(pattern for pattern in node_config['dont_process'] if fnmatch(name, pattern)):
+                should_process = False
 
-                # parent_node will add this node as a child
-                TemplatePageNode(parent_node, name, local_config, target, template)
+            if should_process:
+                # no need to assign this anywhere,
+                # parent_node will add this node as a child.
+                TemplatePageNode(parent_node, name, leaf_config, target, path)
             else:
-                # parent_node will add this node as a child
-                AssetPageNode(parent_node, name, local_config, target, path)
+                AssetPageNode(parent_node, name, leaf_config, target, path)
 
-
+# actual work is done here:
 with open(CONFIG_PATH, 'r') as config_file:
     config = {}
     config.update(DEFAULT_CONFIG)
@@ -90,17 +94,6 @@ with open(CONFIG_PATH, 'r') as config_file:
 
     root_node = FolderNode(None, '', config, folder=DEPLOY_PATH)
 
-    build_templates_in(CONTENT_PATH, DEPLOY_PATH, config, root_node)
+    build_node_tree(CONTENT_PATH, DEPLOY_PATH, config, root_node)
 
     root_node.build(site=root_node)
-    # for template in templates:
-    #     target = template.globals['target']
-
-    #     target_path, target_name = os.path.split(target)
-
-    #     if not os.path.isdir(target_path):
-    #         os.mkdir(target_path, 0755)
-
-    #     content = template.render()
-    #     with open(target, 'w') as dest:
-    #         dest.write(content)
