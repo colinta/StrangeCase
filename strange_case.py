9ac7c05ab659fdb604c58b273a7703386fe8f02a
Colin Thomas-Arnold <colinta@mac.com>
2012-01-19 09:08:33 -0700

    error type in strange_case

--- strange_case.py
+++ strange_case.py
@@ -13,7 +13,7 @@ DEPLOY_PATH = CONFIG['deploy_path']
 if __name__ == '__main__':
     # look for files in content/
     if not os.path.isdir(SITE_PATH):
-        raise "Could not find SITE_PATH folder \"%s\"" % SITE_PATH
+        raise IOError("Could not find SITE_PATH folder \"%s\"" % SITE_PATH)
 
     root_node = Processor.get('root', CONFIG, SITE_PATH, DEPLOY_PATH)[0]
 
