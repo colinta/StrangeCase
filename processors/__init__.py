3bedec74c82a65c4f173ffe945f7ae19d48ba0b2
Colin Thomas-Arnold <colinta@mac.com>
2012-02-19 23:15:19 -0700

    sort listdir *correctly*

--- processors/__init__.py
+++ processors/__init__.py
@@ -60,7 +60,9 @@ def build_node_tree(parent_node, source_path, target_path):
     node_config = parent_node.config_copy()
 
     # scan the folder
-    for file_name in os.listdir(source_path).sort():
+    files = os.listdir(source_path)
+    files.sort()
+    for file_name in files:
         if any(pattern for pattern in node_config['ignore'] if fnmatch(file_name, pattern)):
             continue
 
