5a16cf213839dd912fb74b3834262ac8b5f0d0a2
Colin Thomas-Arnold <colinta@mac.com>
2012-02-23 18:13:38 -0700

    moved template rendering into JinjaNode.render

--- nodes/jinja.py
+++ nodes/jinja.py
@@ -8,11 +8,14 @@ class JinjaNode(PageNode):
     A JinjaNode object is rendered before copied to its destination
     """
     def generate(self, site):
-        template = Registry.get('jinja_environment').get_template(self.source_path)
-        content = template.render(self.config, my=self, site=site)
+        content = self.render(site)
 
         target_path = os.path.join(self.target_folder, self.target_name)
         with open(target_path, 'w') as dest:
             dest.write(content.encode('utf-8'))
 
         super(JinjaNode, self).generate(site)
+
+    def render(self, site=None):
+        template = Registry.get('jinja_environment').get_template(self.source_path)
+        return template.render(self.config, my=self, site=site)
