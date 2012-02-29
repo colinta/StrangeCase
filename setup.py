0335f9abe6a9965d3e651cf9b63ba41a241e3373
Colin Thomas-Arnold <colinta@mac.com>
2012-02-29 14:08:51 -0700

    documentation fix.  lame!

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
         name="StrangeCase",
-        version="v2.1.4",
+        version="v2.1.5",
         author="Colin Thomas-Arnold",
         author_email="colinta.com",
         url="https://github.com/colinta/StrangeCase",
