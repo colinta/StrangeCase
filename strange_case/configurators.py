6a9a5f66c8b45b9c56fe3e454f72cfb6b0735bc8
Colin Thomas-Arnold <colinta@mac.com>
2012-03-29 19:45:38 -0600

    titlecase fix - change '_' to ' '

--- strange_case/configurators.py
+++ strange_case/configurators.py
@@ -234,10 +234,11 @@ def order_from_name(source_file, config):
 
 
 def titlecase(s):
-        return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
-                      lambda mo: mo.group(0)[0].upper() +
-                                 mo.group(0)[1:].lower(),
-                      s)
+    s = s.replace('_', ' ')
+    return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
+                  lambda mo: mo.group(0)[0].upper() +
+                             mo.group(0)[1:].lower(),
+                  s)
 
 
 @provides('title')
