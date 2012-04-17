f7a4dd49fa3810babe7f7f9280508b5c533ca64c
Colin Thomas-Arnold <colinta@gmail.com>
2012-04-17 16:14:53 -0600

    v3.5.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
         name="StrangeCase",
-        version="v3.4.0",
+        version="v3.5.1",
         author="Colin Thomas-Arnold",
         author_email="colinta.com",
         url="https://github.com/colinta/StrangeCase",
