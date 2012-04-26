122008069392843774693545171a59390d4af085
Colin Thomas-Arnold <colinta@gmail.com>
2012-04-26 11:54:51 -0600

    title length check

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
         name="StrangeCase",
-        version="v4.0.4",
+        version="v4.0.5",
         author="Colin Thomas-Arnold",
         author_email="colinta.com",
         url="https://github.com/colinta/StrangeCase",
