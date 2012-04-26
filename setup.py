711832f90f70295480e2686467dca07d4073ce3f
Colin Thomas-Arnold <colinta@gmail.com>
2012-04-26 12:17:44 -0600

    v4.0.6 - markdown fix, and setup.py uses find_packages() - thanks @wichert!

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
         name="StrangeCase",
-        version="v4.0.5",
+        version="v4.0.6",
         author="Colin Thomas-Arnold",
         author_email="colinta.com",
         url="https://github.com/colinta/StrangeCase",
