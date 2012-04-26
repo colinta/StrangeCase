c310821cab35eb366b55c4368335a21ba1d76f86
Colin Thomas-Arnold <colinta@gmail.com>
2012-04-26 16:48:21 -0600

    version bump - my bad.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
         name="StrangeCase",
-        version="v4.0.8",
+        version="v4.0.9",
         author="Colin Thomas-Arnold",
         author_email="colinta.com",
         url="https://github.com/colinta/StrangeCase",
