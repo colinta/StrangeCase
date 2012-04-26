881328c7ade16ccbb18efad5e93665287a7ba7a7
Colin Thomas-Arnold <colinta@gmail.com>
2012-04-25 20:04:58 -0600

    moved the `strange_case` function into __init__.  only run() is in __main__

--- strange_case/__init__.py
+++ strange_case/__init__.py
@@ -3,11 +3,22 @@ from strange_case.extensions import *
 from strange_case.support import *
 from strange_case.nodes import *
 from strange_case.processors import *
-from strange_case.__main__ import run
+
+from fnmatch import fnmatch
+import sys
+import pickle
+
+notifier = None
+try:
+    from gntp import notifier
+    import socket
+except ImportError:
+    pass
+
+from strange_case.processors import build_node
 
 
 def output_error(msg):
-    import sys
     if msg[-1] != "\n":
         msg += "\n"
     sys.stderr.write(msg)
@@ -15,7 +26,6 @@ def output_error(msg):
 
 
 def output_warning(msg):
-    import sys
     if msg[-1] != "\n":
         msg += "\n"
     sys.stderr.write(msg)
@@ -27,3 +37,92 @@ def require_package(pkg):
 
 def recommend_package(pkg):
     output_warning("\033[1m" + pkg + "\033[0m is recommend.\n  > pip install " + pkg)
+
+
+def __scan(folder):
+    ret = []
+    for f in os.listdir(folder):
+        if f.startswith('.'):
+            continue
+
+        f = os.path.join(folder, f)
+        ret.append(f)
+        if os.path.isdir(f):
+            ret.extend(__scan(f))
+    return ret
+
+
+def strange_case(config):
+    # pull out important values.
+    site_path = config['site_path']
+    deploy_path = config['deploy_path']
+
+    # look for files in content/
+    if not os.path.isdir(site_path):
+        raise IOError('Could not find site_path folder "%s"' % site_path)
+
+    # this is the one folder that *doesn't* get processed by processors.build_page_tree,
+    # so it needs special handling here.
+    config.setdefault('type', 'root')
+    Node.files_written = []
+    root_node = build_node(config, site_path, deploy_path, '')[0]
+    Registry.set('root', root_node)
+
+    remove_stale_files = config['remove_stale_files']
+    dont_remove = config['dont_remove']
+    existing_files = []
+    if os.path.isdir(deploy_path):
+        existing_files = __scan(deploy_path)
+    else:
+        os.makedirs(deploy_path, 0755)
+    root_node.generate()
+
+    # create timestamps file
+    timestamps = {}
+    for file_tracked in Node.files_tracked:
+        f = os.path.abspath(file_tracked)
+        timestamps[f] = os.stat(file_tracked).st_mtime
+    timestamps_file = os.path.join(config['project_path'], '.timestamps')
+    pickle.dump(timestamps, open(timestamps_file, 'w'))
+
+    if remove_stale_files and existing_files:
+        paths = []
+        for f in existing_files:
+            if f not in Node.files_written:
+                f = os.path.abspath(f)
+                f_rel = os.path.relpath(f)
+                if any(pattern for pattern in dont_remove if fnmatch(f, pattern)):
+                    sys.stderr.write("\033[32mignoring\033[0m \033[1m" + f_rel + "\033[0m\n")
+                    continue
+
+                if os.path.isdir(f):
+                    paths.insert(0, f)
+                else:
+                    sys.stderr.write("\033[31mrm\033[0m \033[1m" + f_rel + "\033[0m\n")
+                    os.remove(f)
+        # filter out directories that are not empty
+        paths = [p for p in paths if not os.listdir(p)]
+        for p in paths:
+            p_rel = os.path.relpath(p)
+            sys.stderr.write("\033[31mrmdir\033[0m \033[1m" + p_rel + "\033[0m\n")
+            os.removedirs(p)
+
+    if notifier:
+        try:
+            growl = notifier.GrowlNotifier(
+                applicationName="StrangeCase",
+                notifications=["New Messages"],
+                defaultNotifications=["New Messages"],
+            )
+            growl.register()
+
+            # Send one message
+            growl.notify(
+                noteType="New Messages",
+                title="StrangeCase site generated",
+                description="site is available at:\n"
+                    "{config[deploy_path]}"\
+                    .format(config=config),
+            )
+        except socket.error:
+            pass
--- strange_case/__main__.py
+++ strange_case/__main__.py
@@ -3,107 +3,10 @@ import sys
 import yaml
 import pickle
 
-notifier = None
-try:
-    from gntp import notifier
-    import socket
-except ImportError:
-    pass
-
-from fnmatch import fnmatch
 from strange_case.registry import Registry
-from strange_case.processors import build_node
 from strange_case.nodes import Node
 from strange_case.config_dict import ConfigDict
-
-
-def scan(folder):
-    ret = []
-    for f in os.listdir(folder):
-        if f.startswith('.'):
-            continue
-
-        f = os.path.join(folder, f)
-        ret.append(f)
-        if os.path.isdir(f):
-            ret.extend(scan(f))
-    return ret
-
-
-def strange_case(config):
-    # pull out important values.
-    site_path = config['site_path']
-    deploy_path = config['deploy_path']
-
-    # look for files in content/
-    if not os.path.isdir(site_path):
-        raise IOError('Could not find site_path folder "%s"' % site_path)
-
-    # this is the one folder that *doesn't* get processed by processors.build_page_tree,
-    # so it needs special handling here.
-    config.setdefault('type', 'root')
-    Node.files_written = []
-    root_node = build_node(config, site_path, deploy_path, '')[0]
-    Registry.set('root', root_node)
-
-    remove_stale_files = config['remove_stale_files']
-    dont_remove = config['dont_remove']
-    existing_files = []
-    if os.path.isdir(deploy_path):
-        existing_files = scan(deploy_path)
-    else:
-        os.makedirs(deploy_path, 0755)
-    root_node.generate()
-
-    # create timestamps file
-    timestamps = {}
-    for file_tracked in Node.files_tracked:
-        f = os.path.abspath(file_tracked)
-        timestamps[f] = os.stat(file_tracked).st_mtime
-    timestamps_file = os.path.join(config['project_path'], '.timestamps')
-    pickle.dump(timestamps, open(timestamps_file, 'w'))
-
-    if remove_stale_files and existing_files:
-        paths = []
-        for f in existing_files:
-            if f not in Node.files_written:
-                f = os.path.abspath(f)
-                f_rel = os.path.relpath(f)
-                if any(pattern for pattern in dont_remove if fnmatch(f, pattern)):
-                    sys.stderr.write("\033[32mignoring\033[0m \033[1m" + f_rel + "\033[0m\n")
-                    continue
-
-                if os.path.isdir(f):
-                    paths.insert(0, f)
-                else:
-                    sys.stderr.write("\033[31mrm\033[0m \033[1m" + f_rel + "\033[0m\n")
-                    os.remove(f)
-        # filter out directories that are not empty
-        paths = [p for p in paths if not os.listdir(p)]
-        for p in paths:
-            p_rel = os.path.relpath(p)
-            sys.stderr.write("\033[31mrmdir\033[0m \033[1m" + p_rel + "\033[0m\n")
-            os.removedirs(p)
-
-    if notifier:
-        try:
-            growl = notifier.GrowlNotifier(
-                applicationName="StrangeCase",
-                notifications=["New Messages"],
-                defaultNotifications=["New Messages"],
-            )
-            growl.register()
-
-            # Send one message
-            growl.notify(
-                noteType="New Messages",
-                title="StrangeCase site generated",
-                description="site is available at:\n"
-                    "{config[deploy_path]}"\
-                    .format(config=config),
-            )
-        except socket.error:
-            pass
+from strange_case import strange_case
 
 
 def fancy_import(import_name):
@@ -210,7 +113,7 @@ def run():
                 try:
                     extension = fancy_import(extension)
                 except ImportError:
-                    print 'Error in processors: Could not find "%s"' % extension
+                    sys.error.write('Error in processors: Could not find "%s"\n' % extension)
                     raise
             extensions.append(extension)
         del CONFIG['extensions']
@@ -227,7 +130,7 @@ def run():
                 try:
                     method = fancy_import(method)
                 except ImportError:
-                    print 'Error in filters: Could not find "%s"' % method
+                    sys.error.write('Error in filters: Could not find "%s"\n' % method)
                     raise
             jinja_environment.filters[filter_name] = method
         del CONFIG['filters']
@@ -237,7 +140,7 @@ def run():
             try:
                 fancy_import(processor)
             except ImportError:
-                print 'Error in processors: Could not find "%s"' % processor
+                sys.error.write('Error in processors: Could not find "%s"\n' % processor)
                 raise
         del CONFIG['processors']
 
@@ -257,7 +160,7 @@ def run():
                 configurator = fancy_import(configurator)
             configurators.append(configurator)
             Registry.add_configurator(configurator)
-        del CONFIG['configurators']
+        del CONFIG['configurators +']
 
     # additional file_types
     for entry in Registry.file_types:
