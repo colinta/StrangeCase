c29f2b054139ce7eadc0fdf5594a829952e25342
Colin Thomas-Arnold <colinta@gmail.com>
2012-05-01 19:17:34 -0600

    created_at and order can strip their value from name and target_name, and some fixes

--- strange_case/extensions/configurator_created_at_from_name.py
+++ strange_case/extensions/configurator_created_at_from_name.py
@@ -4,10 +4,38 @@ import datetime
 from strange_case.configurators import provides
 
 
-DATE_YMD_RE = re.compile(r'(?P<year>[1-9]\d{3})' + \
-                         r'(?:([-_])(?P<month>\d{2})' + \
-                         r'(?:([-_])(?P<day>\d{2}))?' + \
-                         r')?([-_])(?P<name>.*)')
+CREATED_AT_RE = re.compile(r'''
+    (?P<created_at>
+        (?P<year>[1-9]\d{3})
+        (?:
+            [-_](?P<month>\d{2})
+            (?:
+                [-_](?P<day>\d{2})
+            )?
+        )?
+        [-_]
+    )
+    (?P<name>.*)''', re.VERBOSE)
+
+STRIP_CREATED_AT_RE = re.compile(r'''
+    (?P<created_at>
+        (?P<year>[1-9]\d{3})
+        (?:
+            [-_](?P<month>\d{2})
+            (?:
+                [-_](?P<day>\d{2})
+            )?
+        )?
+        [-_]
+    )
+    ''', re.VERBOSE)
+
+
+def strip_created_at_from(created_at, name):
+    match = STRIP_CREATED_AT_RE.match(name)
+    if match:
+        return name[len(match.group(0)):]
+    return name
 
 
 @provides('created_at')
@@ -17,8 +45,12 @@ def created_at_from_name(source_file, config):
     and you don't have to add `date: ...` using YAML, plus you get a
     python date object.
     """
-    matches = DATE_YMD_RE.search(config['name'])
+    matches = CREATED_AT_RE.match(config['name'])
+    if not matches:
+        matches = CREATED_AT_RE.match(config['target_name'])
+
     if matches:
+        created_at = matches.group('created_at')
         year = int(matches.group('year'))
         if matches.group('month') is not None:
             month = int(matches.group('month'))
@@ -36,20 +68,15 @@ def created_at_from_name(source_file, config):
             day=day,
             )
         config['created_at'] = date
-        if config['strip_from_name']:
-            config['name'] = matches.group('name')
-    else:
-        matches = DATE_YMD_RE.search(config['target_name'])
-        if matches:
-            date = datetime.date(
-                year=int(matches.group('year')),
-                month=int(matches.group('month')),
-                day=int(matches.group('day')),
-                )
-            config['created_at'] = date
+
+        if config['strip_metadata_from_name']:
+            config['name'] = strip_created_at_from(created_at, config['name'])
+
+        if config['strip_metadata_from_target_name']:
+            config['target_name'] = strip_created_at_from(created_at, config['target_name'])
     return config
 
 created_at_from_name.defaults = {
-    'strip_from_name': True,
-    'strip_from_target_name': False,
+    'strip_metadata_from_name': True,
+    'strip_metadata_from_target_name': False,
 }
--- strange_case/extensions/configurator_order_from_name.py
+++ strange_case/extensions/configurator_order_from_name.py
@@ -3,7 +3,19 @@ import re
 from strange_case.configurators import provides
 
 
-INDEX_RE = re.compile(r'(?P<order>[0]\d+)[-_](?P<name>.*)')
+ORDER_RE = re.compile(r'''
+    (?P<order>[0]\d+)
+    [-_]
+    (?P<name>.*)
+''', re.VERBOSE)
+STRIP_ORDER_RE = re.compile(r'(?P<order>[0]\d+)[-_]')
+
+
+def strip_order_from(order, name):
+    match = STRIP_ORDER_RE.match(name)
+    if match:
+        return name[len(match.group(0)):]
+    return name
 
 
 @provides('order')
@@ -12,12 +24,26 @@ def order_from_name(source_file, config):
     Adds ordering to a file name (when dates aren't quite enough).  The first digit
     *must* be a "0", to distinguish it from a date.
     """
-    matches = INDEX_RE.match(config['name'])
+    matches = ORDER_RE.match(config['name'])
+    order = None
     if matches:
-        config['order'] = int(matches.group('order'))
-        config['name'] = matches.group('name')
+        order = int(matches.group('order'))
     else:
-        matches = INDEX_RE.match(config['target_name'])
+        matches = ORDER_RE.match(config['target_name'])
         if matches:
-            config['order'] = matches.group('order')
+            order = matches.group('order')
+
+    if order is not None:
+        config['order'] = order
+
+        if config['strip_metadata_from_name']:
+            config['name'] = strip_order_from(order, config['name'])
+
+        if config['strip_metadata_from_target_name']:
+            config['target_name'] = strip_order_from(order, config['target_name'])
     return config
+
+order_from_name.defaults = {
+    'strip_metadata_from_name': True,
+    'strip_metadata_from_target_name': False,
+}
