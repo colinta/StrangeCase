dce0cb6e54b39a11adc01707b15b0ff8edcc2a0e
Colin Thomas-Arnold <colinta@gmail.com>
2012-05-03 09:17:37 -0600

    fix in date - if `value` is already a date or datetime object, do nothing in `to_datetime`

--- strange_case/extensions/date.py
+++ strange_case/extensions/date.py
@@ -3,18 +3,24 @@ import datetime
 
 
 def to_datetime(value):
+    if isinstance(value, datetime.date) or isinstance(value, datetime.datetime):
+        return value
     if isinstance(value, int) or isinstance(value, float):
         return datetime.datetime.utcfromtimestamp(value)
-    elif isinstance(value, basestring):
+    if isinstance(value, basestring):
         return dateutil.parser.parse(value)
+    raise TypeError('Invalid argument {!r} passed to `to_datetime`'.format(value))
 
 
 def date(value, format='%d %b %Y'):
+    """
+    Default format: 13 May 2012
+    """
     if not value:
         return ''
 
     if value is "now":
-        value = datetime.date.today()
+        value = datetime.datetime.now()
     else:
         value = to_datetime(value)
 
