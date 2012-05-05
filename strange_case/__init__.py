c378a2b2013cb366689a0cd945d6bc86ee3d9eb8
Colin Thomas-Arnold <colinta@gmail.com>
2012-05-05 16:10:02 -0600

    move strange_case config init code into __init__.py : strange_case()

--- strange_case/__init__.py
+++ strange_case/__init__.py
@@ -1,12 +1,13 @@
+from fnmatch import fnmatch
+import sys
+import pickle
+
 from strange_case.registry import Registry
 from strange_case.extensions import *
 from strange_case.support import *
 from strange_case.nodes import *
 from strange_case.processors import *
-
-from fnmatch import fnmatch
-import sys
-import pickle
+from strange_case.nodes import Node
 
 notifier = None
 try:
@@ -14,8 +15,6 @@ try:
     import socket
 except ImportError:
     pass
-
-from strange_case.processors import build_node
 
 
 def output_error(msg):
@@ -39,7 +38,19 @@ def recommend_package(pkg):
     output_warning("\033[1m" + pkg + "\033[0m is recommend.\n  > pip install " + pkg)
 
 
-def __scan(folder):
+def fancy_import(import_name):
+    """
+    This takes a fully qualified object name, like
+    'strange_case.extensions.markdown', and returns the last
+    object.  equivalent to `from strange_case.extensions import markdown`.
+    """
+
+    import_path, import_me = import_name.rsplit('.', 1)
+    imported = __import__(import_path, globals(), locals(), [import_me], -1)
+    return getattr(imported, import_me)
+
+
+def find_files(folder):
     ret = []
     for f in os.listdir(folder):
         if f.startswith('.'):
@@ -48,7 +59,7 @@ def __scan(folder):
         f = os.path.join(folder, f)
         ret.append(f)
         if os.path.isdir(f):
-            ret.extend(__scan(f))
+            ret.extend(find_files(f))
     return ret
 
 
@@ -61,18 +72,70 @@ def strange_case(config):
     if not os.path.isdir(site_path):
         raise IOError('Could not find site_path folder "%s"' % site_path)
 
+    if not os.path.isdir(config['deploy_path']):
+        os.mkdir(config['deploy_path'])
+
+    from strange_case.support.jinja import StrangeCaseEnvironment
+
+    extensions = []
+    if 'extensions' in config:
+        for extension in config['extensions']:
+            if isinstance(extension, basestring):
+                try:
+                    extension = fancy_import(extension)
+                except ImportError:
+                    sys.error.write('Error in processors: Could not find "%s"\n' % extension)
+                    raise
+            extensions.append(extension)
+        del config['extensions']
+
+    if not Registry.get('jinja_environment'):
+        jinja_environment = StrangeCaseEnvironment(extensions=extensions, project_path=config['project_path'])
+        Registry.set('jinja_environment', jinja_environment)
+    else:
+        jinja_environment = Registry.get('jinja_environment')
+
+    if 'filters' in config:
+        for filter_name, method in config['filters'].iteritems():
+            if isinstance(method, basestring):
+                try:
+                    method = fancy_import(method)
+                except ImportError:
+                    sys.error.write('Error in filters: Could not find "%s"\n' % method)
+                    raise
+            jinja_environment.filters[filter_name] = method
+        del config['filters']
+
+    if 'processors' in config:
+        for processor in config['processors']:
+            try:
+                fancy_import(processor)
+            except ImportError:
+                sys.error.write('Error in processors: Could not find "%s"\n' % processor)
+                raise
+        del config['processors']
+
+    # register configurators
+    for configurator in get_configurators(config):
+        Registry.add_configurator(configurator)
+
+    # read timestamps file
+    timestamps_file = os.path.join(config['project_path'], '.timestamps')
+    if os.path.exists(timestamps_file):
+        config['file_mtimes'] = pickle.load(open(timestamps_file))
+
     # this is the one folder that *doesn't* get processed by processors.build_page_tree,
     # so it needs special handling here.
-    config.setdefault('type', 'root')
     Node.files_written = []
     root_node = build_node(config, site_path, deploy_path, '')[0]
+    assert root_node.type == 'root'
     Registry.set('root', root_node)
 
     remove_stale_files = config['remove_stale_files']
     dont_remove = config['dont_remove']
     existing_files = []
     if os.path.isdir(deploy_path):
-        existing_files = __scan(deploy_path)
+        existing_files = find_files(deploy_path)
     else:
         os.makedirs(deploy_path)
     root_node.generate()
@@ -126,3 +189,45 @@ def strange_case(config):
             )
         except socket.error:
             pass
+
+
+def get_configurators(config):
+    configurators = []
+    # load built-in pre configurators
+    for configurator in config['__configurators_pre__']:
+        if isinstance(configurator, basestring):
+            configurator = fancy_import(configurator)
+        configurators.append(configurator)
+    del config['__configurators_pre__']
+
+    # load user configurators
+    if 'configurators' in config:
+        for configurator in config['configurators']:
+            if isinstance(configurator, basestring):
+                configurator = fancy_import(configurator)
+            configurators.append(configurator)
+        del config['configurators']
+
+    # additional configurators, in addition to the all-important defaults
+    if 'configurators +' in config:
+        sys.error.write('''Built-in Configurators are now considered "protected",
+so the "configurators +" hack is no longer needed.
+
+However, you will probably want to make sure to include the defaults:
+- order_from_name
+- created_at_from_name
+- title_from_name''')
+        for configurator in config['configurators +']:
+            if isinstance(configurator, basestring):
+                configurator = fancy_import(configurator)
+            configurators.append(configurator)
+        del config['configurators +']
+
+    # load built-in post configurators
+    for configurator in config['__configurators_post__']:
+        if isinstance(configurator, basestring):
+            configurator = fancy_import(configurator)
+        configurators.append(configurator)
+    del config['__configurators_post__']
+
+    return configurators
--- strange_case/__main__.py
+++ strange_case/__main__.py
@@ -2,23 +2,8 @@ import imp
 import os
 import sys
 import yaml
-import pickle
 
-from strange_case.registry import Registry
-from strange_case.nodes import Node
 from strange_case import strange_case
-
-
-def fancy_import(import_name):
-    """
-    This takes a fully qualified object name, like
-    'strange_case.extensions.markdown', and returns the last
-    object.  equivalent to `from strange_case.extensions import markdown`.
-    """
-
-    import_path, import_me = import_name.rsplit('.', 1)
-    imported = __import__(import_path, globals(), locals(), [import_me], -1)
-    return getattr(imported, import_me)
 
 
 def run():
@@ -116,101 +101,6 @@ def run():
     except AssertionError as e:
         sys.stderr.write("\033[1;31mError:\033[0m \033[1m" + e.message + "\033[0m\n")
         return
-
-    if not os.path.isdir(CONFIG['deploy_path']):
-        os.mkdir(CONFIG['deploy_path'])
-
-    from strange_case.support.jinja import StrangeCaseEnvironment
-
-    extensions = []
-    if 'extensions' in CONFIG:
-        for extension in CONFIG['extensions']:
-            if isinstance(extension, basestring):
-                try:
-                    extension = fancy_import(extension)
-                except ImportError:
-                    sys.error.write('Error in processors: Could not find "%s"\n' % extension)
-                    raise
-            extensions.append(extension)
-        del CONFIG['extensions']
-
-    if not Registry.get('jinja_environment'):
-        jinja_environment = StrangeCaseEnvironment(extensions=extensions, project_path=CONFIG['project_path'])
-        Registry.set('jinja_environment', jinja_environment)
-    else:
-        jinja_environment = Registry.get('jinja_environment')
-
-    if 'filters' in CONFIG:
-        for filter_name, method in CONFIG['filters'].iteritems():
-            if isinstance(method, basestring):
-                try:
-                    method = fancy_import(method)
-                except ImportError:
-                    sys.error.write('Error in filters: Could not find "%s"\n' % method)
-                    raise
-            jinja_environment.filters[filter_name] = method
-        del CONFIG['filters']
-
-    if 'processors' in CONFIG:
-        for processor in CONFIG['processors']:
-            try:
-                fancy_import(processor)
-            except ImportError:
-                sys.error.write('Error in processors: Could not find "%s"\n' % processor)
-                raise
-        del CONFIG['processors']
-
-    configurators = []
-    # load built-in pre configurators
-    for configurator in CONFIG['__configurators_pre__']:
-        if isinstance(configurator, basestring):
-            configurator = fancy_import(configurator)
-        configurators.append(configurator)
-        Registry.add_configurator(configurator)
-    del CONFIG['__configurators_pre__']
-
-    # load user configurators
-    if 'configurators' in CONFIG:
-        for configurator in CONFIG['configurators']:
-            if isinstance(configurator, basestring):
-                configurator = fancy_import(configurator)
-            configurators.append(configurator)
-            Registry.add_configurator(configurator)
-        del CONFIG['configurators']
-
-    # load built-in post configurators
-    for configurator in CONFIG['__configurators_post__']:
-        if isinstance(configurator, basestring):
-            configurator = fancy_import(configurator)
-        configurators.append(configurator)
-        Registry.add_configurator(configurator)
-    del CONFIG['__configurators_post__']
-
-    # additional configurators, in addition to the all-important defaults
-    if 'configurators +' in CONFIG:
-        sys.error.write('''Built-in Configurators are now considered "protected",
-so the "configurators +" hack is no longer needed.
-
-However, you will probably want to make sure to include the defaults:
-- order_from_name
-- created_at_from_name
-- title_from_name''')
-        for configurator in CONFIG['configurators +']:
-            if isinstance(configurator, basestring):
-                configurator = fancy_import(configurator)
-            configurators.append(configurator)
-            Registry.add_configurator(configurator)
-        del CONFIG['configurators +']
-
-    # read timestamps file
-    timestamps_file = os.path.join(CONFIG['project_path'], '.timestamps')
-    if os.path.exists(timestamps_file):
-        CONFIG['file_mtimes'] = pickle.load(open(timestamps_file))
-
-    timestamps = {}
-    for file_tracked in Node.files_tracked:
-        f = os.path.abspath(file_tracked)
-        timestamps[f] = os.stat(file_tracked).st_mtime
 
     if args.watch:
         import time
