332bfca7260974dad2f2d433b68f94eb8fea27c6
Colin Thomas-Arnold <colinta@gmail.com>
2012-05-14 10:11:41 -0600

    test build category site twice

--- strange_case/tests/test_category_site.py
+++ strange_case/tests/test_category_site.py
@@ -31,3 +31,28 @@ def test_category_site(config):
 def test_missing_category_site(config):
     with pytest.raises(NotImplementedError):
         strange_case(config)
+
+
+@will_generate('missing_category_site')
+def test_build_category_site_twice(config):
+    strange_case(config)
+    strange_case(config)
+
+    path_contents = {
+        'index.html': True,
+        'blogs': {
+            'index.html': True,
+            '2012_01_01_post1.html': True,
+            '2012_01_02_post2.html': True,
+        },
+        'zee_categories': {
+            'index.html': [
+                'Categories (2)',
+                '<a href="/zee_categories/good.html">good</a>',
+                '<a href="/zee_categories/bad.html">bad</a>'
+            ],
+            'good.html': ['Category: good', 'Boring category page.'],
+            'bad.html': ['Category: bad', 'This category is bad.'],
+        },
+    }
+    check_path_contents(config['deploy_path'], path_contents)
